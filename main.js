43615a9a09856ca67ec12fe5d022271f6c00f1fe
AmyStark25 <86607872+AmyStark25@users.noreply.github.com>
2021-12-14 19:10:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -36,12 +36,26 @@ function my_keydown(e)
 		otherkey();
 		document.getElementById("d1").innerHTML="You pressed symbol or other key";
 	}
-	if((keyPressed >48 && keyPressed<=57){
+	if((keyPressed >=48 && keyPressed<=57){
 			numberkey();
 			document.getElementById("d1").innerHTML="You pressed the number key!";
 			console.log("number key");
 		}
-
+if((keyPressed >37 && keyPressed<=40){
+			arrowkey();
+			document.getElementById("d1").innerHTML="You pressed the arrow key!";
+			console.log("arrow key");
+		}
+if((keyPressed >48 && keyPressed<=57){
+			numberkey();
+			document.getElementById("d1").innerHTML="You pressed the number key!";
+			console.log("number key");
+		}
+		if((keyPressed = 18)|| (keyPressed = 17) || (keyPressed = 27) ({
+			specialkey();
+			document.getElementById("d1").innerHTML="You pressed the special key!";
+			console.log("special key");
+		}
 }
 
 function alphabetkey()
